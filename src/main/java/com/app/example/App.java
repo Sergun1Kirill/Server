2449acc152c1f626697e37475cd505cfe2ef611e
kirill <kirillsergun@gmail.com>
2024-06-24 23:41:06 +0300

    3 commit

--- src/main/java/com/app/example/App.java
+++ src/main/java/com/app/example/App.java
@@ -20,6 +20,6 @@ public class App extends SpringBootServletInitializer {
 
     @RequestMapping(value = "/")
     public String hello() {
-        return "<center>Hello World Aahhh Mantaapp</center>";
+        return "Hello World>";
     }
 }
